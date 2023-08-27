77bad48b6b7cdb5fe4aacdf85e2640d8fdb02515
AidanFRyan <aidan.ryan4@gmail.com>
2023-08-27 03:36:36 -0400

    Doxygen?

--- engine/kernels.cu
+++ engine/kernels.cu
@@ -2,6 +2,19 @@
 #include "parallelPrefixSumKernels.hu"
 #include "radixSortKernels.hu"
 
+
+/**
+ * @brief find root node containing each particle in domain
+ * 
+ * @param px 
+ * @param py 
+ * @param pz 
+ * @param numParticles 
+ * @param grid 
+ * @param gridPosition 
+ * @return __global__ 
+ */
+
 __global__ void rootCell(float* px, float* py, float* pz, uint numParticles, Grid grid, uint* gridPosition){
     uint index = threadIdx.x + blockIdx.x*blockDim.x;
     if(index < numParticles){
@@ -11,6 +24,23 @@ __global__ void rootCell(float* px, float* py, float* pz, uint numParticles, Gri
         gridPosition[index] = x + y*grid.sizeX + z*grid.sizeX*grid.sizeY;
     }
 }
+
+/**
+ * @brief Find subcell containing each particle inside its containing grid node
+ * 
+ * @param px 
+ * @param py 
+ * @param pz 
+ * @param numParticles 
+ * @param grid 
+ * @param gridPosition 
+ * @param subCellPositionX 
+ * @param subCellPositionY 
+ * @param subCellPositionZ 
+ * @param refinementLevel 
+ * @param xySize 
+ * @return __global__ 
+ */
 
 __global__ void subCell(float* px, float* py, float* pz, uint numParticles, Grid grid, uint* gridPosition, uint* subCellPositionX, uint* subCellPositionY, uint* subCellPositionZ, uint refinementLevel, uint xySize){
     uint index = threadIdx.x + blockIdx.x*blockDim.x;
@@ -32,20 +62,73 @@ __global__ void subCell(float* px, float* py, float* pz, uint numParticles, Grid
     }
 }
 
+/**
+ * @brief wrapper for rootCell
+ * 
+ * @param px 
+ * @param py 
+ * @param pz 
+ * @param numParticles 
+ * @param grid 
+ * @param gridPosition 
+ * @param stream 
+ */
+
 
 void kernels::cudaFindGridCell(float* px, float* py, float* pz, uint numParticles, Grid grid, uint* gridPosition, cudaStream_t stream){
     rootCell<<<numParticles / BLOCKSIZE + 1, BLOCKSIZE, 0, stream>>>(px, py, pz, numParticles, grid, gridPosition);
 }
 
+/**
+ * @brief Wrapper for subCell
+ * 
+ * @param px 
+ * @param py 
+ * @param pz 
+ * @param numParticles 
+ * @param grid 
+ * @param gridPosition 
+ * @param subCellPositionX 
+ * @param subCellPositionY 
+ * @param subCellPositionZ 
+ * @param numRefinementLevels 
+ * @param stream 
+ */
+
 void kernels::cudaFindSubCell(float* px, float* py, float* pz, uint numParticles, Grid grid, uint* gridPosition, uint* subCellPositionX, uint* subCellPositionY, uint* subCellPositionZ, uint numRefinementLevels, cudaStream_t stream){
     subCell<<<numParticles / BLOCKSIZE + 1, BLOCKSIZE, 0, stream>>>(px, py, pz, numParticles, grid, gridPosition, subCellPositionX, subCellPositionY, subCellPositionZ, numRefinementLevels, grid.sizeX*grid.sizeY);
 }
+
+/**
+ * @brief Wrapper for executing CUDA parallel prefix sum
+ * 
+ * @tparam T 
+ * @param numElements 
+ * @param array 
+ * @param blockSums 
+ * @param stream 
+ */
 
 template <typename T>
 void kernels::cudaParallelPrefixSum(uint numElements, T* array, T* blockSums, cudaStream_t stream){
     parallelPrefix<<<numElements/BLOCKSIZE + 1, BLOCKSIZE, 0, stream>>>(numElements, array, blockSums);
     parallelPrefixApplyPreviousBlockSum<<<numElements/BLOCKSIZE + 1, BLOCKSIZE, 0, stream>>>(numElements, array, blockSums);
 }
+
+/**
+ * @brief Wrapper for performing CUDA radix inclusive sort on uint array
+ * 
+ * @param numElements 
+ * @param inArray 
+ * @param outArray 
+ * @param sortedIndices 
+ * @param front 
+ * @param back 
+ * @param blockSumsFront 
+ * @param blockSumsBack 
+ * @param frontStream 
+ * @param backStream 
+ */
 
 void kernels::cudaRadixSortUint(uint numElements, uint* inArray, uint* outArray, uint* sortedIndices, uint* front, uint* back, uint* blockSumsFront, uint* blockSumsBack, cudaStream_t frontStream, cudaStream_t backStream){
     for(uint i = 0; i < sizeof(uint)*8; ++i){
@@ -62,6 +145,14 @@ void kernels::cudaRadixSortUint(uint numElements, uint* inArray, uint* outArray,
         outArray = tempGP;
     }
 }
+
+/**
+ * @brief Sort particles globally by containing root nodes
+ * 
+ * @param numParticles 
+ * @param gridPosition 
+ * @param stream 
+ */
 
 void kernels::cudaSortParticlesByGridNode(uint numParticles, uint*& gridPosition, cudaStream_t stream){
     uint* ogGridPosition = gridPosition;
